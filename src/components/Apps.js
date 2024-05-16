9f6291d1b9010580fe584d5dbc8cd60accb76e29
alypse <44419772+alypse@users.noreply.github.com>
2024-05-16 18:22:23 -0400

    Accordion lists with updated styling. Reduce page length and add author

--- src/components/Apps.js
+++ src/components/Apps.js
@@ -1,17 +1,35 @@
+import { useState } from "react";
 import './Apps.scss';
 import {APPLIST} from "../utils/Applist.js";
 
 
 export function Apps() {
+    const [expanded, setExpanded] = useState(null);
+    const toggle = i => {
+        if (expanded === i) {
+            setExpanded(null);
+
+        }
+        setExpanded(i);
+    };
+
     return (
-            <div className="list">
-                    {APPLIST.apps.map((app) => (
-                        <div key={app.id}>
-                            <a href={app.url} target="_blank" rel="noreferrer">{app.name}</a>
-                            <p>{app.description}</p>
+        <div className="list">
+            {APPLIST.apps.map((app, i) => (
+                <div className='item' key={app.id}>
+                    <div className='title'>
+                        <a className='app' href={app.url} target="_blank" rel="noreferrer">{app.name}</a>
+                        <div className='expander' onClick={() => toggle(i)}>
+                            {expanded === i ? '-' : '+'}
                         </div>
-                    ))}
-            </div>
+                    </div>
+                    <div className={expanded === i ? 'content show' : 'content'}>
+                        <div className='description'>{app.description}</div>
+                        <div className='details'>Author: {app.author}</div>
+                    </div>
+                </div>
+            ))}
+        </div>
     );
 }
 
--- src/components/Guides.js
+++ src/components/Guides.js
@@ -1,17 +1,35 @@
+import {useState} from "react";
 import './Guides.scss';
 import {GUIDELIST} from "../utils/Guidelist.js";
 
 
 export function Guides() {
+    const [expanded, setExpanded] = useState(null);
+    const toggle = i => {
+        if (expanded === i) {
+            setExpanded(null);
+
+        }
+        setExpanded(i);
+    };
+
     return (
-            <div className="list">
-                    {GUIDELIST.guides.map((guide) => (
-                        <div key={guide.id}>
-                            <a href={guide.url} target="_blank" rel="noreferrer">{guide.name}</a>
-                            <p>{guide.description}</p>
+        <div className="list">
+            {GUIDELIST.guides.map((guide, i) => (
+                <div className='item' key={guide.id}>
+                    <div className='title'>
+                        <a className='app' href={guide.url} target="_blank" rel="noreferrer">{guide.name}</a>
+                        <div className='expander' onClick={() => toggle(i)}>
+                            {expanded === i ? '-' : '+'}
                         </div>
-                    ))}
-            </div>
+                    </div>
+                    <div className={expanded === i ? 'content show' : 'content'}>
+                        <div className='description'>{guide.description}</div>
+                        <div className='details'>Author: {guide.author}</div>
+                    </div>
+                </div>
+            ))}
+        </div>
     );
 }
 
