e51caa96b96ee67ef7cc296d12b481581f2af6e0
alypse <44419772+alypse@users.noreply.github.com>
2024-05-25 22:54:42 +0100

    Updated design ready for merge to main

--- src/components/Apps.js
+++ src/components/Apps.js
@@ -1,40 +1,16 @@
-import { useState } from "react";
 import './Apps.scss';
 import { APPLIST } from "../utils/Applist.js";
-import infoCircle  from '../assets/static/info-circle.svg';
+import { PanelApps } from "./PanelApps.js";
 
 // APPLIST.apps.map(app => require(app.icon));  todo: figure out how to require all icons
 
 
 export function Apps() {
-    const [showDialog, setShowDialog] = useState(false);
-    const open = () => setShowDialog(true);
-    const close = () => setShowDialog(false);
-
     return (
         <div className="list">
             {APPLIST.apps.map((app, i) => (
-                <div className='item' key={app.id}>
-                    <div className='title'>
-                        <img className='icon' src={app.icon} alt={app.name}/>
-                        <a className='app' href={app.url} target="_blank" rel="noreferrer">{app.name}</a>
-                        <div className='info'>
-                            <button className='btn' onClick={() => open()}>
-                                <img src={infoCircle} className='info-circle' alt='appinfo'/>
-                            </button>
-                        </div>
-                    </div>
-                    <div>
-                        {showDialog && (
-                            <div className='dialog'>
-                                <div className='dialog-content' onClick={(i) => close()}>
-                                    <div className='name'>{APPLIST.apps[i].name}</div>
-                                    <div className='details'>Created by {APPLIST.apps[i].author}</div>
-                                    <div className='description'>{APPLIST.apps[i].description}</div>
-                                </div>
-                            </div>
-                        )}
-                    </div>
+                <div className='panel' key={app.id}>
+                    <PanelApps app={app}/>
                 </div>
             ))}
         </div>
--- src/components/Guides.js
+++ src/components/Guides.js
@@ -1,32 +1,15 @@
-import {useState} from "react";
 import './Guides.scss';
 import {GUIDELIST} from "../utils/Guidelist.js";
+import { PanelGuides } from "./PanelGuides.js";
 
 
 export function Guides() {
-    const [expanded, setExpanded] = useState(null);
-    const toggle = i => {
-        if (expanded === i) {
-            setExpanded(null);
-
-        }
-        setExpanded(i);
-    };
 
     return (
         <div className="list">
             {GUIDELIST.guides.map((guide, i) => (
-                <div className='item' key={guide.id}>
-                    <div className='title'>
-                        <a className='app' href={guide.url} target="_blank" rel="noreferrer">{guide.name}</a>
-                        <div className='expander' onClick={() => toggle(i)}>
-                            {expanded === i ? '' : '+'}
-                        </div>
-                    </div>
-                    <div className={expanded === i ? 'content show' : 'content'}>
-                        <div className='description'>{guide.description}</div>
-                        <div className='details'>Author: {guide.author}</div>
-                    </div>
+                <div className='panel' key={guide.id}>
+                    <PanelGuides guide={guide}/>
                 </div>
             ))}
         </div>
