2adae0e666def6ed4c0e36c722f2c623c94624f0
alypse <44419772+alypse@users.noreply.github.com>
2024-05-25 14:24:47 +0100

    Add icons and partial figma implementation

--- src/components/Apps.js
+++ src/components/Apps.js
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import './Apps.scss';
-import {APPLIST} from "../utils/Applist.js";
+import { APPLIST } from "../utils/Applist.js";
+import infoCircle  from '../assets/static/info-circle.svg';
+
+// APPLIST.apps.map(app => require(app.icon));  todo: figure out how to require all icons
 
 
 export function Apps() {
     const [expanded, setExpanded] = useState(null);
+
     const toggle = i => {
         if (expanded === i) {
             setExpanded(null);
@@ -18,9 +22,10 @@ export function Apps() {
             {APPLIST.apps.map((app, i) => (
                 <div className='item' key={app.id}>
                     <div className='title'>
+                        <img className='icon' src={app.icon} alt={app.name}/>
                         <a className='app' href={app.url} target="_blank" rel="noreferrer">{app.name}</a>
                         <div className='expander' onClick={() => toggle(i)}>
-                            {expanded === i ? '' : '+'}
+                            {expanded === i ? '' : infoCircle}
                         </div>
                     </div>
                     <div className={expanded === i ? 'content show' : 'content'}>
